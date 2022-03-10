51561c3a26614ee0bdefbef672a812d07a9f9a2b
Mel <meljaynethomas@gmail.com>
2022-03-10 11:37:55 +0000

    first commit - 2 files created containing 3 classes

---
+++ classes_inheritance.py
@@ -0,0 +1,24 @@
+# define classes:
+
+class Person:
+    def __init__(self, first_name, last_name,gender, age):
+        self.name = first_name + last_name
+        self.gender = gender
+        self.age = age
+
+
+class Employee:
+    def __init__(self, job):
+        self.job = job
+
+
+class Customer:
+    def __init__(self, customer_type):
+        self.customer_type = customer_type
+
+
+# Define Methods below:
+
+
+
+
---
+++ instantiation.py
@@ -0,0 +1,4 @@
+from classes_inheritance import Person
+
+person1 = Person("Mel", "Thomas", "Female", 44)
+print(person1.name, person1.gender, person1.age)
